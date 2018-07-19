f145244ccf258e6de5f927921e7d1da7dd883a9a
Edward Zhang <edward@zhang.nz>
2018-07-19 22:15:09 +1200

    Added basic CLI input processing

--- src/main/java/uoa/se306/travellingoliverproblem/Main.java
+++ src/main/java/uoa/se306/travellingoliverproblem/Main.java
@@ -2,6 +2,20 @@ package uoa.se306.travellingoliverproblem;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello bepis world!");
+        if (args.length < 2) {
+            System.err.println("Please provide input task graph and number of processors");
+        }
+
+        if (args.length >= 2) {
+            String filename = args[0];
+            try {
+                int processors = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                System.err.println("Invalid number of processors");
+                System.exit(1);
+            }
+            
+        }
+        // TODO: The rest of the CLI options
     }
 }
