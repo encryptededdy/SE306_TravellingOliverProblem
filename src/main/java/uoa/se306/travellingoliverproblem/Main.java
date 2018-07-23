c0b154fd95ab3db9b33fd5e0a03a143bac1a3321
Edward Zhang <edward@zhang.nz>
2018-07-23 12:28:09 +1200

    Tidied up CLI code

--- src/main/java/uoa/se306/travellingoliverproblem/Main.java
+++ src/main/java/uoa/se306/travellingoliverproblem/Main.java
@@ -13,9 +13,9 @@ public class Main {
         int numOfCores = 1; //default 1 means that the code will run in sequential
         boolean useVisuals = false;
 
-        if (args.length == 0){
+        if (args.length == 0) {
             System.err.println("Please provide input task graph and number of processors.\nType -h or --help for help.");
-        }else if (args.length == 1) {
+        } else if (args.length == 1) {
             String temp = args[0];
             if (temp.equals("-h") || temp.equals("--help")) {
                 System.out.println("java −jar scheduler.jar [INPUT.dot] [P] [OPTION]");
@@ -27,10 +27,10 @@ public class Main {
                 System.out.println("-p --parallel [INTEGER]     The amount of cores to use for execution in parallel.");
                 System.out.println("-o --output [STRING]        Specifies the name for the output file.");
                 System.exit(0);
-            }else{
+            } else {
                 System.err.println("Please provide input task graph and number of processors.\nType -h or --help for help.");
             }
-        }else{
+        } else {
             String fileName = args[0];
 
             try {
@@ -56,26 +56,34 @@ public class Main {
             String outputFileName = file.getName().substring(0, file.getName().lastIndexOf('.')) + "-output.dot";
 
             //check if there are any valid optional arguments
-            if (args.length > 2 ){
-                for (int i = 2; i < args.length ; i++){
+            if (args.length > 2) {
+                for (int i = 2; i < args.length; i++) {
                     String temp = args[i];
 
-                    if (temp.equals("-p") || temp.equals("--parallel")){
-                        numOfCores = Integer.parseInt(args[i+1]);//will throw NumberFormatException if cant convert
-                        i = i + 1;
-                    }else if(temp.equals("-v") || temp.equals("--visualisation")){
-                        useVisuals = true;
-                    }else if(temp.equals("-o") || temp.equals("--output")) {
-                        outputFileName = args[i + 1] + ".dot";
-                        i = i + 1;
-                    }else{
-                        System.err.println("Invalid optional argument(s).\nType -h or --help for help.");
-                        System.exit(1);
+                    switch (temp) {
+                        case "-p":
+                        case "--parallel":
+                            numOfCores = Integer.parseInt(args[i + 1]);//will throw NumberFormatException if cant convert
+
+                            i = i + 1;
+                            break;
+                        case "-v":
+                        case "--visualisation":
+                            useVisuals = true;
+                            break;
+                        case "-o":
+                        case "--output":
+                            outputFileName = args[i + 1] + ".dot";
+                            i = i + 1;
+                            break;
+                        default:
+                            System.err.println("Invalid optional argument(s).\nType -h or --help for help.");
+                            System.exit(1);
                     }
                 }
             }
             //Testing purposes
-            System.out.println("Read graph with "+graph.getStartingNodes().size()+" starting nodes");
+            System.out.println("Read graph with " + graph.getStartingNodes().size() + " starting nodes");
             System.out.println("Number of cores to use: " + Integer.toString(numOfCores));
             System.out.println("Use visuals ? " + String.valueOf(useVisuals));
             System.out.println("The output file name will be: " + outputFileName);
