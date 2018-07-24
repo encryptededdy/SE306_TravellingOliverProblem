82d094fbde518be072039885987c297e7d9bcb06
Edward Zhang <edward@zhang.nz>
2018-07-24 15:39:35 +1200

    Updated Graph to be able to store all nodes within

--- src/main/java/uoa/se306/travellingoliverproblem/graph/Graph.java
+++ src/main/java/uoa/se306/travellingoliverproblem/graph/Graph.java
@@ -1,18 +1,22 @@
 package uoa.se306.travellingoliverproblem.graph;
 
-import java.util.Arrays;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Graph {
     private Set<Node> startingNodes = new HashSet<>(); // should this be made unmodifiable?
 
-    public Graph(Node... nodes) {
-        startingNodes.addAll(Arrays.asList(nodes));
+    private Set<Node> allNodes = new TreeSet<>(); // same as above
+
+    public Graph(Collection<Node> startingNodes, Collection<Node> allNodes) {
+        this.startingNodes.addAll(startingNodes);
+        this.allNodes.addAll(allNodes);
     }
 
-    public Graph(Set<Node> nodes) {
-        startingNodes.addAll(nodes);
+    public Set<Node> getAllNodes() {
+        return allNodes;
     }
 
     public Set<Node> getStartingNodes() {
