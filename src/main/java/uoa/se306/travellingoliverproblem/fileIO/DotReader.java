1de346f0c8f659aedca25fe7ee08b49745bf568e
Edward Zhang <edward@zhang.nz>
2018-07-31 09:58:07 +1200

    Updated unit tests to catch more regex errors, support spaces in graph names

--- src/main/java/uoa/se306/travellingoliverproblem/fileIO/DotReader.java
+++ src/main/java/uoa/se306/travellingoliverproblem/fileIO/DotReader.java
@@ -34,7 +34,7 @@ public class DotReader implements GraphFileReader {
         int lineno = 1;
         try {
             line = br.readLine();
-            if (!line.matches("digraph \\S+ \\{")) {
+            if (!line.matches("digraph .+ \\{")) {
                 throw new InvalidFileFormatException("digraph definition not found");
             }
 
