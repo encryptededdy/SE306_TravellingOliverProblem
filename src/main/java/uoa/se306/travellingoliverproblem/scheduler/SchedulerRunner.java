6b45eeccf1b283d292f6c29e6c8dfc9c5cf6a3df
Edward Zhang <edward@zhang.nz>
2018-08-02 02:23:54 +1200

    Unfinished conversion of ScheduledProcessor to use PriorityQueue

--- src/main/java/uoa/se306/travellingoliverproblem/scheduler/SchedulerRunner.java
+++ src/main/java/uoa/se306/travellingoliverproblem/scheduler/SchedulerRunner.java
@@ -5,6 +5,7 @@ import uoa.se306.travellingoliverproblem.schedule.Schedule;
 import uoa.se306.travellingoliverproblem.schedule.ScheduleEntry;
 import uoa.se306.travellingoliverproblem.schedule.ScheduledProcessor;
 
+import java.util.PriorityQueue;
 import java.util.TreeSet;
 
 public class SchedulerRunner {
@@ -37,7 +38,7 @@ public class SchedulerRunner {
     public void printResult() {
         ScheduledProcessor[] pro = schedule.getProcessors();
         for (int i = 0; i < pro.length; i++) {
-            TreeSet<ScheduleEntry> nodeMap = pro[i].getFullSchedule();
+            PriorityQueue<ScheduleEntry> nodeMap = pro[i].getFullSchedule();
             System.out.println("Processor " + Integer.toString(i) + " has tasks:" + nodeMap.toString());
         }
         System.out.println("The best overall time was: " + schedule.getOverallTime());
