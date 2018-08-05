5e67ff4da4829001e3d6b8918a4e45637462d531
Edward Zhang <edward@zhang.nz>
2018-08-06 01:09:45 +1200

    Removed length (waste of memory!)

--- src/main/java/uoa/se306/travellingoliverproblem/schedule/ScheduleEntry.java
+++ src/main/java/uoa/se306/travellingoliverproblem/schedule/ScheduleEntry.java
@@ -4,27 +4,26 @@ import uoa.se306.travellingoliverproblem.graph.Node;
 
 /*
 A ScheduledEntry is a node that has been scheduled on a processor
+Note this is immutable and thus has no setters, and doesn't need to be deep copied
  */
 public class ScheduleEntry implements Comparable<ScheduleEntry> {
-    private int startTime; //This represents the
-    private int length; //This represents the execution time/cost for this node/task
+    private int startTime;
     private Node node;
 
     // Constructor
     public ScheduleEntry(int startTime, Node node) {
         this.startTime = startTime;
         this.node = node;
-        this.length = node.getCost();
     }
 
     // Returns the start time of the ScheduledEntry
     public int getStartTime() { return startTime; }
 
     // Returns the run time of the ScheduledEntry
-    public int getLength() { return length; }
+    public int getLength() { return node.getCost(); }
 
     // Returns the time at which the ScheduledEntry ended on the processor
-    public int getEndTime() { return startTime + length; }
+    public int getEndTime() { return startTime + node.getCost(); }
 
     // Returns the node the schedule entry is referring to
     public Node getNode() {return this.node;}
@@ -38,6 +37,6 @@ public class ScheduleEntry implements Comparable<ScheduleEntry> {
     // Compare ScheduleEntries based on their end time
     @Override
     public int compareTo(ScheduleEntry o) {
-        return Integer.compare(startTime + length, o.startTime + o.length);
+        return Integer.compare(startTime + node.getCost(), o.startTime + o.node.getCost());
     }
 }
