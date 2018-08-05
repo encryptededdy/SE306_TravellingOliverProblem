5b6cfb8d61061f8d6dadd17c88cfdcbfb8d92db7
bcox280 <blairacox@gmail.com>
2018-08-05 17:03:49 +1200

    Add test cases that our client has defined

--- src/test/java/se306/travellingoliverproblem/TestOptimalSolutions.java
+++ src/test/java/se306/travellingoliverproblem/TestOptimalSolutions.java
@@ -71,15 +71,104 @@ public class TestOptimalSolutions {
     }
 
     @Test
-    public void test10NodesRandom() throws FileNotFoundException {
+    public void test7OutTree2Processors() throws FileNotFoundException {
 
-        Graph inputGraph = getGraphFromInput("10NodesRandom.dot");
-        // 2 starting nodes
-        assertEquals(inputGraph.getStartingNodes().size(), 2);
+        Graph inputGraph = getGraphFromInput("Nodes_7_OutTree.dot");
 
-        Scheduler scheduler = new DFSScheduler(inputGraph, 3);
+        Scheduler scheduler = new DFSScheduler(inputGraph, 2);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 28);
+    }
+
+    @Test
+    public void test7OutTree4Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_7_OutTree.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 4);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 22);
+    }
+
+    @Test
+    public void test8Random2Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_8_Random.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 2);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 581);
+    }
+
+    @Test
+    public void test8Random4Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_8_Random.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 4);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 581);
+    }
+
+
+    @Test
+    public void test9SeriesParallel2Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_9_SeriesParallel.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 2);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 55);
+    }
+
+    @Test
+    public void test9SeriesParallel4Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_9_SeriesParallel.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 4);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 55);
+    }
+
+    @Test
+    public void test10NodesRandom2Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_10_Random.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 2);
 
         checkValidAndOptimal(scheduler.getBestSchedule(), 50);
+    }
+
+    @Test
+    public void test10NodesRandom4Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_10_Random.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 2);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 50);
+    }
+
+    @Test
+    public void test11OutTree2Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_11_OutTree.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 2);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 350);
+    }
+
+    @Test
+    public void test11OutTree4Processors() throws FileNotFoundException {
+
+        Graph inputGraph = getGraphFromInput("Nodes_11_OutTree.dot");
+
+        Scheduler scheduler = new DFSScheduler(inputGraph, 4);
+
+        checkValidAndOptimal(scheduler.getBestSchedule(), 227);
     }
 
 
