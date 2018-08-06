752a152910b7ef7756e39c591272417d67ceff78
Benny Chun <bennychun97@gmail.com>
2018-08-06 23:07:41 +1200

    Skeleton code for cost function done

--- src/main/java/uoa/se306/travellingoliverproblem/scheduler/AStarSearchScheduler.java
+++ src/main/java/uoa/se306/travellingoliverproblem/scheduler/AStarSearchScheduler.java
@@ -2,11 +2,15 @@ package uoa.se306.travellingoliverproblem.scheduler;
 
 import uoa.se306.travellingoliverproblem.graph.Graph;
 import uoa.se306.travellingoliverproblem.schedule.Schedule;
+import uoa.se306.travellingoliverproblem.scheduler.heuristics.CostFunction;
 
 /*
 Scheduler for the A Star Algorithm
  */
 public class AStarSearchScheduler extends Scheduler {
+
+    private long totalCost;
+
 
     public AStarSearchScheduler(Graph graph, int amountOfProcessors) {
         super(graph, amountOfProcessors);
@@ -14,8 +18,7 @@ public class AStarSearchScheduler extends Scheduler {
 
     @Override
     protected void calculateSchedule(Schedule currentSchedule) {
-
-
-
+        CostFunction costFunction = new CostFunction();
+        totalCost = costFunction.calculateCost();
     }
 }
--- src/main/java/uoa/se306/travellingoliverproblem/scheduler/heuristics/CostFunction.java
+++ src/main/java/uoa/se306/travellingoliverproblem/scheduler/heuristics/CostFunction.java
@@ -1,4 +1,34 @@
 package uoa.se306.travellingoliverproblem.scheduler.heuristics;
 
 public class CostFunction {
+    private long totalIdleTime;
+    private long totalNumberOfProcessors;
+    private long dataReadyTime;
+    private long totalCost;
+
+    public long calculateCost(){
+        totalIdleTime = calculateTotalIdleTime();
+        totalNumberOfProcessors = calculateTotalNumberOfProcessors();
+        dataReadyTime = calculateDataReadyTime();
+
+        totalCost = totalIdleTime + totalNumberOfProcessors + dataReadyTime;
+
+        return totalCost;
+    }
+
+    public long calculateTotalNumberOfProcessors(){
+
+        return totalNumberOfProcessors;
+    }
+
+    public long calculateDataReadyTime(){
+
+        return dataReadyTime;
+    }
+
+    public long calculateTotalIdleTime(){
+
+        return totalIdleTime;
+    }
+
 }
