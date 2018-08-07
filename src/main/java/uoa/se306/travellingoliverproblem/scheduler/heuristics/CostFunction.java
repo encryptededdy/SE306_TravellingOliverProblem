a105f031a537311defb84c3bf4376d79533056c0
Jack <pathicalmine@gmail.com>
2018-08-07 16:50:36 +1200

    added some commenting in CostFunction

--- src/main/java/uoa/se306/travellingoliverproblem/scheduler/heuristics/CostFunction.java
+++ src/main/java/uoa/se306/travellingoliverproblem/scheduler/heuristics/CostFunction.java
@@ -1,39 +1,68 @@
 package uoa.se306.travellingoliverproblem.scheduler.heuristics;
 
+import uoa.se306.travellingoliverproblem.graph.Node;
 import uoa.se306.travellingoliverproblem.schedule.Schedule;
 import uoa.se306.travellingoliverproblem.schedule.ScheduleEntry;
 import uoa.se306.travellingoliverproblem.schedule.ScheduledProcessor;
 
+import java.util.HashMap;
+
 public class CostFunction {
     private Schedule partialSchedule;
+    // we don't need this
+    HashMap<Node, Integer> bottomLevelMap;
+    // From the partial schedule
+    // we can get the node.getBottomLevel()
+
     private long maxStartTimeAndBottomLevel;
     private float idleTimeAndComputation;
     private long maxDataReadyTimeAndBottomLevel;
 
 
-    public CostFunction(Schedule partialSchedule){
+    public CostFunction(Schedule partialSchedule , HashMap<Node, Integer> bottomLevelMap){
         this.partialSchedule = partialSchedule;
+        this.bottomLevelMap = bottomLevelMap;
     }
 
+    /*
+    This method calls all heuristic methods and returns the maximum cost of those methods
+     */
     public float calculateCost(){
         maxDataReadyTimeAndBottomLevel();
         maxStartTimeAndBottomLevel();
         idleTimeAndComputation();
-        return  Math.max(maxDataReadyTimeAndBottomLevel, Math.max(idleTimeAndComputation, maxStartTimeAndBottomLevel));
+        return  Math.max(idleTimeAndComputation, Math.max(maxDataReadyTimeAndBottomLevel, maxStartTimeAndBottomLevel));
     }
 
-    public void maxStartTimeAndBottomLevel(){
+    /*
+    The first cost function principle is to find the node (already scheduled in a processor)
+    That has the maximum (start time + bottom level).
+    This method saves this value as maxStartTimeAndBottomLevel.
+     */
+    private void maxStartTimeAndBottomLevel(){
         maxStartTimeAndBottomLevel = 0;
     }
 
-    public void idleTimeAndComputation(){
+    /*
+    The second cost function principle is to calculate the total idle time of the given schedule
+    plus (total computational load / processors)
+     */
+    private void idleTimeAndComputation(){
+        int totalIdelTime = 0;
+        int totalComputationTime = 0;
         ScheduledProcessor[] processors = partialSchedule.getProcessors();
         for(ScheduledProcessor p : processors){
-            // get idle time of each processor
+            // for each processor,
         }
+
+
     }
 
-    public void maxDataReadyTimeAndBottomLevel(){
+    /*
+    The third cost function principle is to find the node (available to be scheduled)
+    that has the maximum (starting time of node + bottom level)
+     */
+    private void maxDataReadyTimeAndBottomLevel(){
         ScheduledProcessor[] processors = partialSchedule.getProcessors();
         for(ScheduledProcessor p : processors){
             // get the last end time
