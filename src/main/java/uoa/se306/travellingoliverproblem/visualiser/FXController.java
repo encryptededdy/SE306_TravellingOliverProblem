04c33f6a64a98867bfce40cdc5f11f1e57e4d0cd
Edward Zhang <edward@zhang.nz>
2018-08-18 21:47:50 +1200

    Allows for scheduler picking, fixed bug in HybridScheduler

--- src/main/java/uoa/se306/travellingoliverproblem/visualiser/FXController.java
+++ src/main/java/uoa/se306/travellingoliverproblem/visualiser/FXController.java
@@ -19,6 +19,7 @@ import uoa.se306.travellingoliverproblem.graph.Graph;
 import uoa.se306.travellingoliverproblem.graph.Node;
 import uoa.se306.travellingoliverproblem.schedule.Schedule;
 import uoa.se306.travellingoliverproblem.scheduler.SchedulerRunner;
+import uoa.se306.travellingoliverproblem.scheduler.SchedulerType;
 import uoa.se306.travellingoliverproblem.visualiser.graph.GraphDrawer;
 import uoa.se306.travellingoliverproblem.visualiser.graph.GraphNode;
 import uoa.se306.travellingoliverproblem.visualiser.schedule.ScheduleDrawer;
@@ -51,8 +52,8 @@ public class FXController {
         graphNodeMap = drawer.getGraphNodes();
     }
 
-    public void startProcessing(Graph inputGraph, int processors, String outputName) {
-        Task<Void> task = SchedulerRunner.getInstance().startSchedulerJavaFXTask(inputGraph, processors);
+    public void startProcessing(Graph inputGraph, int processors, String outputName, SchedulerType type) {
+        Task<Void> task = SchedulerRunner.getInstance().startSchedulerJavaFXTask(inputGraph, processors, type);
         drawGraph(SchedulerRunner.getInstance().getInputGraph());
         long startTime = System.nanoTime();
 
